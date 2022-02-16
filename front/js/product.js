81b30610dad78f450575a7218c0bf114e20aef54
Karim Farhi <ka.farhi@gmail.com>
2022-02-16 18:42:44 +0100

    ajout des couleurs

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+// Id du produit issue de l'API
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 console.log(id);
@@ -5,6 +6,7 @@ console.log(document.location);
 
 getArticle();
 
+// Affichage du produit
 function getArticle(article) {
   fetch("http://localhost:3000/api/products/" + id)
     .then((response) =>
@@ -12,13 +14,19 @@ function getArticle(article) {
         .then((data) => {
           console.log(data);
           testArticle = data;
-          console.log(testArticle.price);
+          // Affichage des caractéristiques du produit via l'API : prix, nom, description, image
           document.getElementById("price").innerHTML = testArticle.price;
           document.getElementById("title").innerHTML = testArticle.name;
           document.getElementById("description").innerHTML = testArticle.description;
           document.querySelector(".item__img").innerHTML = `<img src="${testArticle.imageUrl}">`;
+          // Boucle affichage des couleurs
+          for (let colors of testArticle.colors) {
+            let colorProduct = document.createElement("option");
+            document.querySelector("#colors").appendChild(colorProduct);
+            colorProduct.value = colors;
+            colorProduct.innerHTML = colors;
+          }
+
         }))
 }
-
-
 
